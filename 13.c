46034510db5ccac471b0e581e579fef51688ca72
kimdaehee0824 <0824dh@naver.com>
2021-03-24 16:14:58 +0900

    https://www.acmicpc.net/problem/10871

--- 13.c
+++ 13.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 int main()
 {
-    int i, a, num = 0;
-    scanf("%d", &num);
+    int i, a, x, num = 0;
+    scanf("%d %d", &num, &x);
     int max = 0;
     for ( i = 0; i <= num; i++)
     {
         scanf("%d", &a);
-        if (max <= a)
+        if (x > a)
         {
-            max = a;
+            printf("%d ", a);
         }
     }
-    printf("%d", max);
     
     return 0;
 }
