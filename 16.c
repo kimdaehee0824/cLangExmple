81ebfaff28b68805180469d27bf298f83cdb8946
김대희 <0824dh@naver.com>
2021-03-29 16:34:33 +0900

    codeup1358

--- 16.c
+++ 16.c
@@ -1,25 +1,21 @@
-//codeup1357
+//codeup1358
 #include <stdio.h>
 int main()
 {
     int i;
     int count;
     scanf("%d", &count);
-
-    for (i = 1; i <= count; i++)
+    for (i = 1; i <= count / 2 + 1; i++)
     {
-        for (int j = 1; j <= i; j++)
+        for (int j = (count + 1) / 2 - i; j >= 1; j--)
+        {
+            printf(" ");
+        }
+        for (int j = 1; j <= 2 * i - 1; j++)
         {
             printf("*");
         }
         printf("\n");
     }
-    for (i = 1; i <= count; i++)
-    {
-        for (int j = count - 1; j >= i; j--)
-        {
-            printf("*");
-        }
-        printf("\n");
-    }
+    return 0;
 }
