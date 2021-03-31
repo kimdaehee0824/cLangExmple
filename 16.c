c1f55c0d0a5ab5a6bea68bb6f7d7a54bc22fd58f
김대희 <0824dh@naver.com>
2021-03-31 14:59:17 +0900

    백준 2522

--- 16.c
+++ 16.c
@@ -17,6 +17,20 @@ int main()
         }
         printf("\n");
     }
-   
+    for (int i = 1; i <= count; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            printf(" ");
+        }
+        for (int j = count; j >= i + 1; j--)
+        {
+            printf("*");
+        }
+
+       
+        printf("\n");
+    }
+
     return 0;
 }
