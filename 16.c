abb3245965f8742bb4777d8c24b75615744d3dc4
김대희 <0824dh@naver.com>
2021-03-31 15:38:24 +0900

    백준 2444

--- 16.c
+++ 16.c
@@ -3,7 +3,7 @@ int main()
 {
     int count;
     scanf("%d", &count);
-    for (int i = 1; i <= count; i++)
+    for (int i = 1; i <= count - 1; i++)
     {
         for (int j = count; j >= i + 1; j--)
         {
@@ -17,7 +17,25 @@ int main()
 
         for (int j = 0; j < i - 1; j++)
         {
-            printf("d");
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    for (int i = 1; i <= count; i++)
+    {
+        for (int j = 1; j <= i - 1; j++)
+        {
+            printf(" ");
+        }
+
+        for (int j = count; j >= i; j--)
+        {
+            printf("*");
+        }
+        for (int j = count; j >= i + 1; j--)
+        {
+            printf("*");
         }
         printf("\n");
     }
