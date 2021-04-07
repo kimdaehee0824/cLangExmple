2281feccfed541e6f82919cf1af378ce2ce45fe9
김대희 <0824dh@naver.com>
2021-04-07 15:33:30 +0900

    10818 최소, 최대

--- 18.c
+++ 18.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
 int main()
 {
-    char str[18] = "Nice to meet you!";
-    printf("배열 str의 크기 : %d\n", sizeof(str));
-    printf("NULL 문자 문자형 출력 : %c\n", str[17]);
-    printf("NULL 문자 정수형 출력 : %d\n", str[17]);
-    str[16] = '?';
-    printf("문자열 출력 : %s\n", str);
-    printf("\n");
+    int i, x;
+    scanf("%d", &x);
+    int nArr[1000000];
+    int max = -1000000, min = 10000000;
     
-    char nu = '\0';
-    char sp = ' ';
-    printf("%d %d\n", nu, sp);
+    for (i = 0; i < x; i++)
+    {
+        scanf("%d", &nArr[i]);
+
+        if(max < nArr[i])
+            max = nArr[i];
+        if(min > nArr[i])
+            min = nArr[i];
+    }
+
+    printf("%d %d", min, max);
+
 
     return 0;
 }
