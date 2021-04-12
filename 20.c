1ceb6481b32a3f33b6ed004ad49603792836630b
김대희 <0824dh@naver.com>
2021-04-12 14:58:20 +0900

    [EDIT] codeup 1468

--- 20.c
+++ 20.c
@@ -1,19 +1,29 @@
 #include <stdio.h>
 int main()
 {
-    int arr1[100][100];
+    int arr1[100][100] = {0};
     int n, m, num = 1;
-    scanf("%d %d", &n, &m);
-    for (int j = m - 1; j >= 0; j--)
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
     {
-        for (int i = 0; i < n; i++)
+        if (i % 2 == 0)
         {
-            arr1[i][j] = num++;
+            for (int j = 0; j < n; j++)
+            {
+                arr1[i][j] = num++;
+            }
+        }
+        else
+        {
+            for (int j = n - 1; j >= 0; j--)
+            {
+                arr1[i][j] = num++;
+            }
         }
     }
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < m; j++)
+        for (int j = 0; j < n; j++)
         {
             printf("%d ", arr1[i][j]);
         }
