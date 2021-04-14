5397d60f0fb140c98a7faed92d2323cb7a24756e
김대희 <0824dh@naver.com>
2021-04-14 14:54:00 +0900

    [Modify] 20.c

--- 20.c
+++ 20.c
@@ -3,29 +3,28 @@ int main()
 {
     int arr1[100][100] = {0};
     int n, m, num = 1;
-    scanf("%d", &n);
-    for (int j = 0; j < n; j++)
+    scanf("%d %d", &n, &m);
+
+    for (int i = n - 1; i >= 0; i--)
     {
-        if (j % 2 == 0)
+        if ((n - i) % 2 == 0)
         {
-            for (int i = n - 1; i >= 0; i--)
+            for (int j = 0; j < m; j++)
             {
                 arr1[i][j] = num++;
             }
         }
         else
         {
-            for (int i = 0; i < n; i++)
+            for (int j = m - 1; j >= 0; j--)
             {
                 arr1[i][j] = num++;
             }
-            
-            
         }
     }
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n; j++)
+        for (int j = 0; j < m; j++)
         {
             printf("%d ", arr1[i][j]);
         }
