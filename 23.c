8629cefc629d02a5240c846cbaa35d7be9bd12b1
김대희 <0824dh@naver.com>
2021-04-27 16:53:11 +0900

    [Mofify] 수정

--- 23.c
+++ 23.c
@@ -1,28 +1,7 @@
 #include <stdio.h>
 int main()
 {
-    int i, j, arr[] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-    int len = sizeof(arr) / sizeof(int), tmp;
-    int *fptr = arr, *bptr = arr + len - 1;
-    for (i = 0; i < len / 2; i++)
-    {
-        printf("*ftpr :%2d, *bptr :%2d [ ", *fptr, *bptr);
-        tmp = *fptr;
-        *fptr = *bptr;
-        *bptr = tmp;
+   int arr[6] = 0-=
 
-        fptr++;
-        bptr--;
-        for (j = 0; j < len; j++)
-        {
-            printf("%d ", *(arr + j));
-        }
-        printf("]\n");
-    }
-    printf("\n");
-    for (i = 0; i < len; i++)
-    {
-        printf("%d ", *(arr + i));
-    }
     return 0;
 }
