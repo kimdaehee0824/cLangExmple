2d0891b2c9d076f3c386065cf36bbf19f59b27f7
김대희 <0824dh@naver.com>
2021-05-17 15:43:10 +0900

    [Modify] 34.c

--- 34.c
+++ 34.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
+
 void add(int val);
 int num;
 int main()
 {
     printf("num : %d\n", num);
-    Add(3);
+    add(3);
     printf("num : %d\n", num);
-    
+
     num++; // 전역변수 num의 1 증가 
     printf("num : %d\n", num);
     return 0;
 }
 
-void Add(int val)
+void add(int val)
 {
     num += val;
 }
