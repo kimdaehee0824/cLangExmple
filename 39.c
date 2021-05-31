58c667785e88b1e46b85fa201e15726ac5608d7f
김대희 <0824dh@naver.com>
2021-05-31 14:34:34 +0900

    [Modify] struct

--- 39.c
+++ 39.c
@@ -2,28 +2,28 @@
 #include <string.h>
 struct Person // struct Person 구조체 선언
 {
-    double height;
-    double weight;
-    char name[10];
-    short grade;
+    char 1;
+    short 2;
+    char 3;
+    int 4;
+    double 5;
+    char 6;
+};
+struct Person2 // struct Person 구조체 선언
+{
+    char 1;
+    char 3;
+    short 2;
+    int 4;
+    double 5;
+    char 6;
 };
 
 int main()
 {
     // struct Person 구조체 변수 person 선언
     struct Person person;
-    // 구조체 멤버 접근 및 값 채우기
-    person.height = 174.2;
-    person.weight = 67.8;
-    person.grade = 1;
-    strcpy(person.name, "홍길동");
-
-    // 구조체 멤버 접근 및 출력
-
-    printf("person.height : %.1lf\n", person.height);
-    printf("person.weight : %.1lf\n", person.weight);
-    printf("person.name : %s\n", person.name);
-    printf("person.grade : %d\n", person.grade);
-    
+    struct Person2 person2;
+    3
     return 0;
 }
--- 40.c
+++ 40.c
@@ -1,30 +1,28 @@
 #include <stdio.h>
-typedef struct Person_
+typedef struct Employee
 {
-    double height;
-    double weight;
     char name[10];
+    char userNumber;
+    int cost;
+} Employee;
 
-    short grade;
-} Person;
 int main()
 {
     // struct Person 구조체 변수 person 선언
-    Person person;
+    Employee person;
     // 멤버 입력받기
-    printf("이름 :");
+    printf("이름 : ");
     scanf("%s", person.name);
-    printf("키 :");
-    scanf("%lf", &person.height);
-    printf("몸무게 :");
-    scanf("%lf", &person.weight);
-    printf("등급 :");
-    scanf("%hd", &person.grade);
-    // 입력받은 구조체 데이터 출력
+    printf("주민번호 : ");
+    scanf("%s", &person.userNumber);
+    printf(" : ");
+    scanf("%d", &person.cost);
+    
     printf("\n데이터 출력\n");
-    printf("키 : %.1lf\n", person.height);
-    printf("몸무게 : %.1lf\n", person.weight);
-    printf("이름 : %s\n", person.name);
-    printf("등급 : %d\n", person.grade);
+    printf("person.height : %.1lf\n", person.height);
+    printf("person.weight : %.1lf\n", person.weight);
+    printf("person.name : %s\n", person.name);
+    printf("person.grade : %d\n", person.grade);
+
     return 0;
 }
