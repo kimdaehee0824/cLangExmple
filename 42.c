defe0bb8e03d54296444e84c240f802d4278eecc
김대희 <0824dh@naver.com>
2021-07-02 09:10:19 +0900

    https://www.acmicpc.net/problem/10808

--- 42.c
+++ 42.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 int main()
 {
-    char arr[100] = {};
+    char s[100] = "";
+    int count[26] = {0};
+    scanf("%s", s);
     for (int i = 0; i < 100; i++)
     {
-        while
-            scanf("%[^\n]s", arr[i]) != EOF;
+        count[s[i] - 'a']++;
+    }
+    for (int i = 0; i < 26; i++)
+    {
+        printf("%d ", count[i]);
     }
     
     return 0;
