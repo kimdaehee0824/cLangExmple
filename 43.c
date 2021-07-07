8c8220420983d855325f2499ee0786f87a504024
김대희 <0824dh@naver.com>
2021-07-07 14:53:46 +0900

    문자열을 덧붙이는 함수

--- 43.c
+++ 43.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 int main()
 {
-    int ch1, ch2;
-    printf("문자 입력 :");
-    ch1 = getchar();   
-    ch2 = fgetc(stdin);
-
-    if (ch1 >= 'a' && ch1 <= 'z')
-    {
-        printf("문자 출력 :");
-        putchar(ch1 - 32);
-        fputc(ch2, stdout);
-    } else 
-        printf("범위를 벗어난 입력입니다");
-        
+    char str1[20] = "First~";
+    char str2[20] = "Second";
+    char str3[20] = "123456789";
+    char str4[20] = "SimpleNum";
+    char *pstr;
+    // case 1
+        strcat(str1, str2);
+        puts(str1);
+    // case 2
+        strncat(str3, str4, 7);
+        puts(str3);
+    pstr = (char *)malloc(sizeof(str3) * strlen(str3) + sizeof(str4) * strlen(str4));
+    strcpy(pstr, str3);
+    strcat(pstr, str4);
+    puts(pstr);
+    free(pstr);
     return 0;
 }
